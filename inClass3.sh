d2d645ca16d3565001e67180d2f417b0b523b204
CompBio <lbazar@lsu.edu>
2018-10-02 10:18:09 -0700

    In class assignment part 3.

---
+++ inClass3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#(3) Write a script that reads in two columns of numbers from a file. The columns are separated by a tab. Have it output any lines where the first column contains a number greater than 10 and the second column contains a number less than 10.
+
+echo "Enter name of file with two tab delimited columns."
+
+read filename
+
+cat $filename | awk '{ if(($1>10) && ($2<10)){print}}'
+
+exit
