c559015f5fa380c3fde03cebc865fbf1203890f6
jembrown <jeremymbrown@gmail.com>
2018-12-06 15:08:41 -0600

    Comments added

--- scriptWeek5/dateAnalysis.sh
+++ scriptWeek5/dateAnalysis.sh
@@ -7,6 +7,8 @@ a=2018
 for number in $@
 do
 	echo "The following number,"$@", is the year being searched."
+	# DB: I think you want this line to be:
+	#     echo "The following number,"$number", is the year being searched."
 done
 
 if  [ $@ -ne $a ]
@@ -16,3 +18,5 @@ else
         echo "There is only data from the year searched."
 fi
 
+# DB: Good idea, but your if statement breaks if the user provides more than one year
+#     on the command line.
