6552395bbcd66c535a84a98d5dd2ecaca738942c
jembrown <jeremymbrown@gmail.com>
2018-12-08 12:35:01 -0600

    Updating week 6 comments

--- inClass2.sh
+++ inClass2.sh
@@ -8,5 +8,4 @@ read fileName
 
 cat $fileName | ./inClass1.sh
 
-# DB: This is an interesting idea, but won't work with your approach for inClass1.sh. It
-#     would work, however, if structured it like this: ./inClass1.sh `cat $fileName`.
+# DB: Good! Creative.
